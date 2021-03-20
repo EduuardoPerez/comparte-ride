081ae512f4aa7d2c83c86c9b5f8a15867e7d4fff
Eduardo Perez <edujpp1@gmail.com>
2021-03-20 11:11:15 -0300

    The users serializers module was included into the users app

--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 
 # Serializers
-from cride.serializers.users import UserLoginSerializer
+from cride.users.serializers import UserLoginSerializer
 
 
 class UserLoginAPIView(APIView):
